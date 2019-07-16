c0894387f45269e5af0a7c7d21fe82b9a7576c13
LeeHyunJu <dlguswn7175@ajou.ac.kr>
2019-07-16 10:59:43 +0900

    영화 기본 레이아웃 4 - OnTouchListener와 MotionEvent를 이용한 event처리방법. ActionUp에서 performClick을 추가해야 하는점을 유의하자.

--- app/src/main/java/com/example/myandroidproject/MainActivity.java
+++ app/src/main/java/com/example/myandroidproject/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.myandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView1;
     TextView textView2;
+    Button thumbupButton;
+    Button thumbdownButton;
+    TextView test;
+    View view1;
     int switchFrame = 0;
 
     @Override
@@ -19,6 +28,37 @@ public class MainActivity extends AppCompatActivity {
 
         textView1 = (TextView) findViewById(R.id.bluecolor);
         textView2 = (TextView) findViewById(R.id.greencolor);
+        thumbupButton = (Button) findViewById(R.id.thumbup);
+        thumbdownButton = (Button) findViewById(R.id.thumbdown);
+        test = (TextView) findViewById(R.id.test);
+        view1 = (View) findViewById(R.id.view1);
+
+        view1.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int action = motionEvent.getAction();
+
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    println("손가락 눌림 : " + curX + ", " + curY);
+                } /*else if (action == MotionEvent.ACTION_MOVE) {
+                    println("손가락 움직임 : " + curX + ", " + curY);
+                }*/ else if (action == MotionEvent.ACTION_UP) {
+                    println("손가락 뗌 : " + curX + ", " + curY);
+                    view.performClick();
+                }
+
+                return true;
+            }
+        });
+    }
+
+    public void println(String str){
+        test.append(str);
     }
 
     public void onButton1Clicked(View v){
@@ -32,5 +72,14 @@ public class MainActivity extends AppCompatActivity {
             switchFrame = 0;
         }
     }
+
+    public void onThumbupButtonClicked(View v){
+
+    }
+
+    public void onThumbdownButtonClicked(View v){
+
+    }
+
 }
 
